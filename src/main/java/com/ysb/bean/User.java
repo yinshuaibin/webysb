10238931a41c0569657881a02cbbef5a0f2b60af
yinshuaibin <35283085+yinshuaibin@users.noreply.github.com>
2021-11-17 21:44:47 +0800

    user实现序列化接口,删除pom中不使用的包

--- src/main/java/com/ysb/bean/User.java
+++ src/main/java/com/ysb/bean/User.java
@@ -2,13 +2,15 @@ package com.ysb.bean;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author yinshuaibin
  * @date 2021/3/19 11:55
  * @description
  */
 @Data
-public class User {
+public class User implements Serializable {
     private int id;
     private String username;
     private String password;
